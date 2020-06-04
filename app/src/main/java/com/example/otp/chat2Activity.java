20d8e30946651fba75a819d6b7d38da004d3a0a3
Bhupendrapatel98 <bhupendrapatel.bp57@gmail.com>
2020-06-04 22:01:05 +0530

    login complited

--- app/src/main/java/com/example/otp/chat2Activity.java
+++ app/src/main/java/com/example/otp/chat2Activity.java
@@ -2,7 +2,9 @@ package com.example.otp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class chat2Activity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class chat2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat2);
+
+        Handler a=new Handler();
+        a.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent a=new Intent(chat2Activity.this,MainActivity.class);
+                startActivity(a);
+                finish();
+
+            }
+        },2000);
+
     }
 }
--- app/src/main/java/com/example/otp/chatActivity.java
+++ app/src/main/java/com/example/otp/chatActivity.java
@@ -8,22 +8,12 @@ import android.view.View;
 import android.widget.TextView;
 
 public class chatActivity extends AppCompatActivity {
-    TextView text;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
-
-        text=findViewById(R.id.text);
-        text.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent a=new Intent(chatActivity.this,chat2Activity.class);
-                startActivity(a);
-                finish();
-            }
-        });
     }
 }
